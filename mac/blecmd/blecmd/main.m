aaf94afe2e3edf509e9ffd7800588ed1b02b9b85
Robert <rzheng@xtremeprog.com>
2014-05-27 09:45:02 +0800

    mac cmdline programe

---
+++ mac/blecmd/blecmd/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  blecmd
+//
+//  Created by Robert Zheng on 14-5-27.
+//  Copyright (c) 2014年 mycabinet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
