40182c3248b15f95dc60f5b13c2de4ab0aa4f444
Robert <rzheng@xtremeprog.com>
2014-06-14 18:37:26 +0800

    add a class in xib

---
+++ mac/TextApp/TextApp/NotifyingClass.h
@@ -0,0 +1,13 @@
+//
+//  NotifyingClass.h
+//  TextApp
+//
+//  Created by Robert Zheng on 14-6-14.
+//  Copyright (c) 2014年 mycabinet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NotifyingClass : NSObject
+
+@end
---
+++ mac/TextApp/TextApp/NotifyingClass.m
@@ -0,0 +1,13 @@
+//
+//  NotifyingClass.m
+//  TextApp
+//
+//  Created by Robert Zheng on 14-6-14.
+//  Copyright (c) 2014年 mycabinet. All rights reserved.
+//
+
+#import "NotifyingClass.h"
+
+@implementation NotifyingClass
+
+@end
